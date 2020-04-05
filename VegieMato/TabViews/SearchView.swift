5ed5ce4ecf8c6af0213e166ec1984bb749374915
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-04-05 19:50:26 +0400

    Added Search

--- VegieMato/TabViews/SearchView.swift
+++ VegieMato/TabViews/SearchView.swift
@@ -9,8 +9,22 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText: String = ""
+    
     var body: some View {
-        Text("Coming Soon!")
+        NavigationView {
+            VStack {
+                SearchBar(text: $searchText)
+                List(vendors.filter {
+                    searchText.isEmpty ? true : $0.name.contains(searchText)
+                }) { vendor in
+                    NavigationLink(destination: VendorView(vendor: vendor)) {
+                        VendorRow(vendor: vendor)
+                    }
+                }
+                .navigationBarTitle(Text("Vendors"))
+            }
+        }
     }
 }
 
