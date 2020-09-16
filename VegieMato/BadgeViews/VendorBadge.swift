c8a6336041753acd56ba7627eed2e37ab7aa6edb
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-09-16 09:22:23 +0400

    removed tests

---
+++ VegieMato/BadgeViews/VendorBadge.swift
@@ -0,0 +1,21 @@
+//
+//  VendorBadge.swift
+//  VegieMato
+//
+//  Created by Rehatbir Singh on 09/16/2020.
+//  Copyright © 2020 MysteryCoder456. All rights reserved.
+//
+
+import SwiftUI
+
+struct VendorBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VendorBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        VendorBadge()
+    }
+}
