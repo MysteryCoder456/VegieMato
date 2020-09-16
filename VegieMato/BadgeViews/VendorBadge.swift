1fdf55da7f1fd121945d75be9bcac94b31c1a7f3
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-09-16 10:42:19 +0400

    added VendorItem for new UI in home screen

--- VegieMato/BadgeViews/VendorBadge.swift
+++ VegieMato/BadgeViews/VendorBadge.swift
@@ -9,13 +9,32 @@
 import SwiftUI
 
 struct VendorBadge: View {
+    var vendor: Vendor
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 15) {
+            Image(vendor.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15, antialiased: true)
+                .frame(height: 200)
+            
+            VStack(alignment: .leading, spacing: 3) {
+                Text(vendor.name)
+                    .foregroundColor(.black)
+                
+                Text(vendor.tagline)
+                    .foregroundColor(.gray)
+                    .font(.caption)
+            }
+        }
+        .padding(.trailing, 20)
+        .shadow(radius: 10)
     }
 }
 
 struct VendorBadge_Previews: PreviewProvider {
     static var previews: some View {
-        VendorBadge()
+        VendorBadge(vendor: defaultVendor)
     }
 }
