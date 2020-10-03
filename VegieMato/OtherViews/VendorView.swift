56bad8183ab35399965da403aca0d32e2d730030
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-10-03 21:16:55 +0400

    moved add product button

--- VegieMato/OtherViews/VendorView.swift
+++ VegieMato/OtherViews/VendorView.swift
@@ -14,11 +14,21 @@ struct VendorView: View {
     @State var showAddPorductView = false
     
     var body: some View {
-        VStack() {
-            if self.showAddPorductView && Auth.auth().currentUser?.uid == vendor.ownerID {
-                AddProductView(vendor: vendor)
-            } else {
-                ScrollView(.vertical) {
+        HStack {
+            ScrollView(.vertical) {
+                if Auth.auth().currentUser?.uid == vendor.ownerID {
+                    HStack {
+                        Spacer()
+                        Button(action: { self.showAddPorductView.toggle() }) {
+                            Image(systemName: "plus.circle.fill")
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                
+                if self.showAddPorductView && Auth.auth().currentUser?.uid == vendor.ownerID {
+                    AddProductView(vendor: vendor)
+                } else {
                     VStack(alignment: .center) {
                         ImageView(imageName: vendor.imageName, width: 100, height: 100)
                             .padding(.top, -10.0)
@@ -43,12 +53,6 @@ struct VendorView: View {
                         }
                         
                         Divider()
-                    }
-                    
-                    if Auth.auth().currentUser?.uid == vendor.ownerID {
-                        Button(action: {self.showAddPorductView = true}) {
-                            Text("Add Product")
-                        }
                     }
                     
                     Spacer()
